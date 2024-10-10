a94ad2695d33387b733fc4c98b3e79a6d3a0815d
Kuulimagnaat <kaarelparve@gmail.com>
2024-10-10 22:04:47 +0300

    Algusfailid cglearnist

---
+++ shaders/chopper.vert.glsl
@@ -0,0 +1,14 @@
+#version 400
+
+uniform mat4 projectionMatrix;
+uniform mat4 viewMatrix;
+uniform mat4 modelMatrix;
+
+in vec3 position;
+in vec3 color;
+out vec3 interpolatedColor;
+
+void main(void) {
+    interpolatedColor = color;
+    gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+}
