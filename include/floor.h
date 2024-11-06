acccb66bb0395d27d8f0a8eceba9f3f101a7cf11
Kuulimagnaat <kaarelparve@gmail.com>
2024-11-06 20:49:13 +0200

    Seinaloogika

--- include/floor.h
+++ include/floor.h
@@ -1,4 +1,6 @@
+#pragma once
 #include "shader_util.h"
+
 GLuint initWalls(shader_prog shader);
 GLuint createWall(glm::vec3 color, float s, shader_prog shader);
 void drawFloor(GLuint floorVAO, shader_prog shader);
